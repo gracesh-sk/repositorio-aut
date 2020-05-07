90475f05bfc2e30e70e719a8d2356daca4d83fb0
gracesh-sk <huamangr@gmail.com>
2020-05-07 16:20:58 -0500

    Se agrega el documento Readme.md

---
+++ Suma.java
@@ -0,0 +1,13 @@
+public class Suma{
+  public static void main(String args[]){
+   
+   int num1 = 5;
+   int num2 = 2;
+   int resultado = 0;
+   
+   resultado = num1 + num2;
+   System.out.println("El resultado de la suma es: " +resultado);
+   
+   
+ }
+}
